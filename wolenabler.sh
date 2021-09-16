0b97673c03036c2828d6ca086a88d9ea91db565a
Błażej Drozd <59662074+Tsugumik@users.noreply.github.com>
2021-09-16 19:23:41 +0200

    Create wolenabler.sh

---
+++ wolenabler.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+interfaces=()
+for iface in $(ifconfig | cut -d ' ' -f1| tr ':' '\n' | awk NF)
+do
+        if [ "$iface" != "lo" ]
+        then
+                interfaces+=("$iface")
+        fi
+done
+for iface in "{$interfaces[@]}"
+do
+        /sbin/ethtool -s $iface wol g
+done
